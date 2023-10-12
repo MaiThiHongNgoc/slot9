d997857126d87221bf14bd55fcbcc41b5384f8cd
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2023-10-12 17:05:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let id =(id) => document.getElementById(id);
-let classes =(classes) => document.getElementsByClassName(classes);
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
 
 let username = id("username"),
   email = id("email"),
@@ -9,27 +10,28 @@ let username = id("username"),
   successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-form.addEventListener("submit" ,(e) => {
-    e.preventDefault();
-
-    engine(username, 0, "Username cannot be black");
-    engine(email, 1, "Email cannot be blank");
-    engine(password, 2, "Password cannot be black");
+  engine(username, 0, "Username cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
 });
-let engine = (id, serial, mesage) => {
-    if (id.value.trim() === "") {
-        errorMsg[serial].innerHTML = message;
-        id.style.border = "2px solid red";
 
-        failureIcon[serial].style.opacity ="1";
-        successIcon[serial].style.opacity = "0";
-    } elsse {
-        errorMsg[serial].innerHTML ="";
-        id.style.border ="2px solid green";
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
 
+    // icons
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
 
-        failureIcon[serial].style.opacity="0";
-        successIcon[serial].style.opacity="1";
-    }
+    // icons
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
 };
